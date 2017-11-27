49606a788c2b9ac65e222df06738d9a4133885fa
Aurelien Dobbels <adobbels@gmail.com>
2017-11-27 19:29:32 +0100

    remove has attacment in models

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,8 +1,6 @@
 class Flat < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
-  has_attachment :photo
-
   belongs_to :profile #when a user is the owner of a flat
   has_many :reviews
   has_many :rents
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,6 @@
 class Profile < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
-  has_attachment :photo
-
   belongs_to :user
   has_many :flats
   has_many :reviews
