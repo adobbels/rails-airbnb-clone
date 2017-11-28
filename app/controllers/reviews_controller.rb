58baa101fd376c1017bfe7d33810c4cc980b9e01
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-11-28 17:09:25 +0100

    ajout du lien pour créer un commentaire + controller review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,16 @@
 class ReviewsController < ApplicationController
 
   def new           # GET /profiles/new
+    @flat = Flat.find(params[:flat_id])
+    @review = Review.new
   end
 
   def create        # POST /profiles
+    @review = Review.new(review_params)
+    @flat = Flat.find(params[:flat_id])
+    @review.flat = @flat
+    @review.save
   end
-
 
 private
   def review_params
