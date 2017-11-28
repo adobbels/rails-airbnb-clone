f8e59a6de053e12bb92a390d14ea42523bd7813b
Antoine STAUMONT <staumont.antoine@me.com>
2017-11-28 18:19:02 +0100

    User_profile OK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :has_profile?
+  def has_profile?
+    if current_user && current_user.profile.nil?
+      redirect_to new_user_profile_path(current_user)
+    end
+  end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,31 +1,51 @@
 class ProfilesController < ApplicationController
-
+skip_before_action :has_profile?
   def index         # GET /profiles
+    @profiles = Profile.all
   end
 
   def show          # GET /profiles/:id
+    @profile = Profile.find(current_user)
   end
 
   def new           # GET /profiles/new
+    @profile = Profile.new
+    @profile.user = current_user
   end
 
   def create        # POST /profiles
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save
+      flash[:notice] = 'Profile was successfully created.'
+      redirect_to user_profile_path(@profile, current_user)
+    else
+      flash[:notice] = 'Error.  Something went wrong.'
+      render :new
+    end
   end
 
   def edit          # GET /profiles/:id/edit
+    @profile = Profile.find(current_user)
   end
 
   def update        # PATCH /profiles/:id
+    @profile = Profile.find(current_user)
+    @profile.update(profile_params)
+    redirect_to user_profile_path(@profile, current_user)
   end
 
   def destroy       # DELETE /profiles/:id
+    @profile = Profile.find(current_user)
+    @profile.destroy
+    # redirect
   end
 
 private
 
 def profile_params
-  # To be updated
-  # params.require(:profile).permit(:name, :description, :photo, :photo_cache)
+
+  params.require(:profile).permit(:first_name, :last_name, :address, :city, :post_code, :phone_number, :sex, :birth_date )
 end
 
 end
