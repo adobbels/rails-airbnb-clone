55d5467c574b61027dec41d78b5314bdf9dc3265
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-11-30 00:40:12 +0100

    creation des routes en resources mais non nestées + creation de quelques methodes

--- app/controllers/flat_options_controller.rb
+++ app/controllers/flat_options_controller.rb
@@ -1,8 +1,23 @@
 class FlatOptionsController < ApplicationController
+  def index
+  end
+
   def new
+    @flat = Flat.find(params[:flat_id])
+    @flat_option = FlatOption.new
   end
 
   def create
+    @feature =
+    @flat = Flat.find(params[:flat_id])
+    @flat_option = FlatOption.new(flat_option_params)
+    @flat_option.flat = @flat
+    @flat_otiopn.feature = @feature
+    if @review.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def update
@@ -17,6 +32,10 @@ class FlatOptionsController < ApplicationController
   def show
   end
 
-  def index
+  private
+  def flat_option_params
+    params.require(:flat_option).permit(:content, :title_review)
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'flat_options/new'
-
-  get 'flat_options/create'
-
-  get 'flat_options/update'
-
-  get 'flat_options/edit'
-
-  get 'flat_options/destroy'
-
-  get 'flat_options/show'
-
-  get 'flat_options/index'
-
   devise_for :users
   resources :users do
     resources :profiles
@@ -22,6 +8,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create ]
     resources :rents
   end
+  resources :flat_options
 
   root to: 'pages#home'
 end
