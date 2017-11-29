a171fea4dc0a6d36475cd3cdbc4c3e5de4cb8c3d
thomas hunckler <hunckler.thomas@hotmail.fr>
2017-11-30 00:33:07 +0100

    creation du controller flat options

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'flat_options/new'
+
+  get 'flat_options/create'
+
+  get 'flat_options/update'
+
+  get 'flat_options/edit'
+
+  get 'flat_options/destroy'
+
+  get 'flat_options/show'
+
+  get 'flat_options/index'
+
   devise_for :users
   resources :users do
     resources :profiles
